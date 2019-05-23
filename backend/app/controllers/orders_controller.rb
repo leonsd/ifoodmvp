b74d9a0108f2641728ec5e44fd7ced6903903c58
leonsd <leon.sdsilva@gmail.com>
2019-05-22 21:56:45 -0300

    correction create order return

--- backend/app/controllers/orders_controller.rb
+++ backend/app/controllers/orders_controller.rb
@@ -1,10 +1,12 @@
+# frozen_string_literal: true
+
 class OrdersController < ApplicationController
   before_action :set_order, only: :show
 
   def create
     order = Order.new(order_params)
     if order.save
-      render json: @order, status: :created
+      render json: order, status: :created
     else
       render json: order.errors, status: :unprocessable_entity
     end
